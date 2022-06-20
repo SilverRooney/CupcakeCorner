0e913c143ee877b8a5aaaa7370de61cbc2bfba30
SilverRooney <silver.rooney.dev@gmail.com>
2022-06-20 12:34:07 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Silver on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
